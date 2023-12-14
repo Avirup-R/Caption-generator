150362973720715440425bb7972c378bdf714a2e
Avirup Rakshit <79531488+Avirup-R@users.noreply.github.com>
2023-12-14 17:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import gradio as gr
 import re 
 from transformers import AutoTokenizer, ViTFeatureExtractor, VisionEncoderDecoderModel
 from bardapi import Bard
+from bardapi import BardCookies
 
 
-device='cpu'
+device='cuda'
 encoder_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
 decoder_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
 model_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
@@ -22,23 +23,30 @@ def listtostr(li):
 
 
 def get_text(prompt):
-  token = 'WghfRvtVO_W5Xh8J_q0gM-xlln4FspcCVg-qGbgxvNa0CSayl6eCwxykYGXoBg3Srbu78g.'
-  bard = Bard(token=token)
-  ans=bard.get_answer("create 5 catchy captions for '"+ prompt+"' with numbering as caption1 and so on with the caption on the same line")['content']
+  cookie_dict = {
+    "__Secure-1PSID": "your tokens",
+    "__Secure-1PSIDTS": "your tokens",
+    "__Secure-1PSIDCC": "your tokens"}
+  bard = BardCookies(cookie_dict=cookie_dict)
+  token = ''
+  ans=bard.get_answer("create 5 catchy captions for '"+ prompt+"' with numbering as caption1 and so on with the caption on the same line. Please return only text no images.")['content']
   return ans
 
 
 def predict(image,max_length=64, num_beams=4):
   image = image.convert('RGB')
   image = feature_extractor(image, return_tensors="pt").pixel_values.to(device)
+
   clean_text = lambda x: x.replace('<|endoftext|>','').split('\n')[0]
   caption_ids = model.generate(image, max_length = max_length)[0]
   caption_text = clean_text(tokenizer.decode(caption_ids))
+  print(caption_text)
   value=get_text(caption_text)
   text=value.splitlines()
   count=0
   no=1
   content=[]
+  print(value)
   for i in text:
      new=str(no)
      temp=str('Caption '+ new)
@@ -47,7 +55,10 @@ def predict(image,max_length=64, num_beams=4):
         count+=1
         no+=1
   captions=listtostr(content)
-  return captions
+  if len(captions)==0:
+     return value
+  else:
+    return captions
 
     
   # return all_captions 
@@ -73,10 +84,10 @@ img#style-image {
 demo = gr.Blocks(css=css)
 with demo:
   gr.Markdown('''<h1 id="title">Image Caption Generator.</h1>''')
-  gr.Markdown('''Made by : Avirup Rakshit''')
+  # gr.Markdown('''Made by : Avirup Rakshit''')
   with gr.Column():
-        input = gr.inputs.Image(label="Upload your Image", type = 'pil', optional=True)
-        output = gr.outputs.Textbox(type="text", label="Captions")
+        input = gr.Image(label="Upload your Image", type = 'pil')
+        output = gr.Textbox(type="text", label="Captions")
   btn = gr.Button("Genrate Caption")
   btn.click(fn=predict, inputs=input, outputs=output)
-demo.launch()
+demo.launch(share=False)
